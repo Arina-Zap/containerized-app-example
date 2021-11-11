c0da16f8e74a3c046f1404a7263031f1cba5cfd9
arzapo <arinazaporozhetc@gmail.com>
2021-11-11 12:58:08 +0200

    refactoring of java file

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     String index() {
-        return "Hello World!";
+        return "Welcome to DevOps crash course!";
     }
 
     public static void main(String[] args) {
